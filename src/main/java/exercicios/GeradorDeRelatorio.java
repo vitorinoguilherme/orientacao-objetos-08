e625e9c194624c1c3250f9a54c6fc291b2369016
Guilherme Vitorino <guilherme.vitorino@indraenergia.com>
2020-09-16 22:59:22 -0300

    Implementado classe GeradorDeRelatorio

---
+++ src/main/java/exercicios/GeradorDeRelatorio.java
@@ -0,0 +1,8 @@
+package exercicios;
+
+public class GeradorDeRelatorio {
+    public void adiciona(EmpregadoDaFaculdade f) {
+        System.out.println(f.getInfo());
+        System.out.println(f.getGastos());
+    }
+}
