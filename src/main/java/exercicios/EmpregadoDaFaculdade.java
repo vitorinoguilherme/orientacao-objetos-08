b483972401259315ffe9b5576cc3211af631e674
Guilherme Vitorino <guilherme.vitorino@indraenergia.com>
2020-09-16 22:58:34 -0300

    Implementado classe EmpregadoDaFaculdade

---
+++ src/main/java/exercicios/EmpregadoDaFaculdade.java
@@ -0,0 +1,18 @@
+package exercicios;
+
+public class EmpregadoDaFaculdade {
+    private String nome;
+    private double salario;
+
+    public double getGastos() {
+        return this.salario;
+    }
+
+    public String getInfo() {
+        return "nome: " + this.nome + "com salário " + this.salario;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+}
